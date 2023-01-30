d77596e5f109fe4a389b826a305d7ece5f31053c
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-30 23:07:42 +0100

    Create Register.js

---
+++ src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <div>
+            <div>
+                <h1>Register</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Register
