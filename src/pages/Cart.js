00b32ee330a9c9de50bb24d6c1bf4f89ad65d55e
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-30 20:26:12 +0100

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import {Button, Container, Col, Row, Table} from 'react-bootstrap'
+import {useCart} from 'react-use-cart'
+import { useThemeHook } from '../GlobalComponents/ThemeProvider'
+import { BsCartCheck, BsCartX } from 'react-icons/bs'
+
 
 const Cart = () => {
+    const [theme] = useThemeHook ();
+    const {
+        isEmpty,
+        totaUniqueItem,
+        totalItems,
+        items
+    } = useCart();
+    
+    
     return (
         <div style={{height : '100vh', width : '100vw', background : 'green'}}>
             Cart Page
