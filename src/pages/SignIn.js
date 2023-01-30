27ada81b72b01403c20c6915cda37665af861671
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-30 23:29:41 +0100

    Update SignIn.js

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Container, Row, Col, Button, From, Spinner,InputGroup } from 'react-bootstrap'
+import { useThemeHook } from '../GlobalComponents/ThemeProvider'
+import {Link} from '@reach/router'
 
 const SignIn = () => {
+    
+    const [loading, setLoading] = useState(false);
+    const [theme] = useThemeHook();
+    
     return (
         <div>
-            SignIn
+            
         </div>
     )
 }
