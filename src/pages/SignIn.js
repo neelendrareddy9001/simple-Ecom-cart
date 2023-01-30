b417d9f8ddfabdfae193b19d6e1f2b6bda7c6f2c
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-30 23:42:34 +0100

    Update SignIn.js

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -9,9 +9,21 @@ const SignIn = () => {
     const [theme] = useThemeHook();
     
     return (
-        <div>
-            
-        </div>
+       <Container className='py-5 mt-5'>
+            <Row className='justify-content-center mt-5'>
+                <Col xs={11} sm={10} md={8} lg={4} className={`p-4 rounded ${theme? 'text-light bg-dark' : 'text-black bg-light'}`}>
+                    <h1 className={`text-center border-bottom pb-3 ${theme? 'text-dark-primary' : 'text-light-primary'}`}>
+                        Sign In
+                    </h1>
+                    <Form>
+                        <InputGroup className='mb-4 mt-5'>
+                            <InputGroup.Text>@</InputGroup.Text>
+                            <Form.Control name="username" type="text" placeholder='Username' minLength={3}required/>
+                        </InputGroup>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>  
     )
 }
 
