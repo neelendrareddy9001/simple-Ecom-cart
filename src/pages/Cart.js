7059dad2f0057b1257e35a478f287940c81e676a
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-30 20:51:40 +0100

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -24,6 +24,38 @@ const Cart = () => {
             <h1 className={`${theme? 'text-light' : 'text-light-primary'} my-5 text-center`}>
                {isEmpty? 'your Cart is' : 'The Cart'} 
             </h1>
+            <Row className='justify-content-center'>
+            <Table responsive="sm" striped bordered hover variant={theme? 'dark': 'light'} className="mb-5">
+                    <tbody>
+                        {items.map((item, index)=>{
+                            return(
+                                <tr key={index}>
+                                    <td>
+                                        <div style={{ background: 'white', height: '8rem', overflow: 'hidden', display: 'flex',
+                                        justifyContent: 'center', alignItems: 'center' }}>
+                                            <div style={{ padding: '.5rem'}}>
+                                                <img src={item.image} style={{ width: '4rem'}} alt={item.title} />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <h6 style={{ whiteSpace: 'nowrap', width: '14rem', overflow: 'hidden', textOverFlow: 'ellipsis'}}>
+                                            {item.title}
+                                        </h6>
+                                    </td>
+                                    <td>Rs. {item.price}</td>
+                                    <td>Quantity ({item.quantity})</td>
+                                    <td>
+                                        <Button onClick={()=> updateItemQuantity(item.id, item.quantity - 1)} className="ms-2">-</Button>
+                                        <Button onClick={()=> updateItemQuantity(item.id, item.quantity + 1)} className="ms-2">+</Button>
+                                        <Button variant="danger" onClick={()=> removeItem(item.id)} className="ms-2">Remove Item</Button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+            </Row>
         </Container>
     )
 }
