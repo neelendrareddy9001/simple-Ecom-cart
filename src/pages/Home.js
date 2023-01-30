93dd123ac7c712d7d86dcab573a6c699e20e8778
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-30 11:26:42 +0100

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div style={{height : '100vh', width : '100vw', background : 'red'}}>
+            Home Page
+        </div>
+    )
+}
+
+export default Home
