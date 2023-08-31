fdce35f1f922b5010fb67c5ef940f4324e53199d
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-08-31 21:27:57 +0200

    Home component and react-filter-search package installed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
+import { useThemeHook } from "../GlobalComponents/ThemeProvider";
+import { BiSearch } from "react-icons/bi";
+import SearchFilter from "react-filter-search";
 
 const Home = () => {
-    return (
-        <div style={{height : '100vh', width : '100vw', background : 'red'}}>
-            Home Page
-        </div>
-    )
-}
+  const [theme] = useThemeHook();
+  const [searchInput, setSearchInput] = useState();
+  const [productData, setProductData] = useState([]);
 
-export default Home
+  async function getResponse() {
+    const res = fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }
+  useEffect(() => {
+    getResponse();
+    console.log(productData);
+  }, []);
+  return (
+    <Container className="py-4">
+      <Row className="justify-content-center">
+        <Col xs={10} md={7} lg={6} xl={4} className="mb-3 mx-auto text-center">
+          <h1 className={theme ? "text-light my-5" : "text-black my-5"}>
+            Search Product
+          </h1>
+          <InputGroup className="mb-3">
+            <InputGroup.Text
+              className={
+                theme
+                  ? "bg-black text-dark-primary"
+                  : "bg-light text-light-primary"
+              }
+            >
+              <BiSearch />
+            </InputGroup.Text>
+            <FormControl
+              placeholder="Search"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              className={
+                theme
+                  ? "bg-light-black text-light"
+                  : "bg-light-text-light-primary"
+              }
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
